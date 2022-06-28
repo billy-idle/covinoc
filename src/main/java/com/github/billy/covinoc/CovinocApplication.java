c57c3c26922cb866ea87bb063f61dce91283ad1a
Billy <guillesupremacy@duck.com>
2022-06-27 20:56:46 -0500

    Master branch set up!

--- src/main/java/com/github/billy/covinoc/CovinocApplication.java
+++ src/main/java/com/github/billy/covinoc/CovinocApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CovinocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CovinocApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CovinocApplication.class, args);
+  }
 }
